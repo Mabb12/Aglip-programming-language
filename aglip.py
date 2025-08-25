7db3e25358d52c325d79e75f489211ee59117f58
Mabb12 <umaruk2809@gmail.com>
2025-08-25 16:39:55 +0600

    Update aglip.py

--- aglip.py
+++ aglip.py
@@ -3,15 +3,15 @@ def compile_aglip(code):
     in_loop = 0
     counter = 0
     for cmd in code:
-        if cmd == "A": py_code += "    " * in_loop + "memory[counter] = (memory[counter] + 1) % 257\n" # Add
+        if cmd == "A": py_code += "    " * in_loop + "memory[counter] = (memory[counter] + 1) % 256\n" # Add
         elif cmd == "G": # Go
-            py_code += "    " * in_loop + "counter = (counter + 1) % 257\n"
-            counter += 1 % 257
+            py_code += "    " * in_loop + "counter = (counter + 1) % 256\n"
+            counter += 1 % 256
         elif cmd == "L": # Loop
             if in_loop == 0:
                 py_code += "    " * in_loop + f"while memory[{counter}] != memory[{counter + 1}]:\n"
             in_loop = 1 - in_loop
-        elif cmd == "I": py_code += "    " * in_loop + "memory[counter] = int(input()) % 257\n" # Input
+        elif cmd == "I": py_code += "    " * in_loop + "memory[counter] = int(input()) % 256\n" # Input
         elif cmd == "P": py_code += "    " * in_loop + "print(memory[counter])\n" # Print
     return py_code
 
@@ -19,4 +19,5 @@ if __name__ == "__main__":
     program = 'AGLIP'
     python_code = compile_aglip(program)
     print(python_code)
+
     exec(python_code)
